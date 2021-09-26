e56b5c8c7ae86770a3cc3218b8609471c8703f3a
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-09-26 17:20:02 +0530

    waiter changes

--- routes/waiter.js
+++ routes/waiter.js
@@ -10,7 +10,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-router.get("/waiterLog", auth, async (req, res) => {
+router.get("/me", auth, async (req, res) => {
   const waiter = await Waiter.findById(req.waiter._id).select("-password");
   res.send(waiter);
 });
@@ -25,7 +25,14 @@ router.post("/signup", async (req, res) => {
     let user = await Waiter.findOne({ email: req.body.email });
     if (user) return res.status(400).json("Waiter already registered.");
     user = new Waiter(
-      _.pick(req.body, ["name", "email", "password", "nic", "contactNo","salary"])
+      _.pick(req.body, [
+        "name",
+        "email",
+        "password",
+        "nic",
+        "contactNo",
+        "salary",
+      ])
     );
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
@@ -34,15 +41,12 @@ router.post("/signup", async (req, res) => {
     const token = user.generateAuthToken();
     res
       .header("x-auth-token", token)
-      .json(_.pick(user, ["_id", "name", "email", "nic", "contactNo","salary"]));
+      .json(
+        _.pick(user, ["_id", "name", "email", "nic", "contactNo", "salary"])
+      );
   } catch (err) {
     return res.status(422).json(err.message);
   }
 });
 
 module.exports = router;
-
-// router.get("/me", auth, async (req, res) => {
-//   const waiter = await Waiter.findById(req.waiter._id).select("-password");
-//   res.send(waiter);
-// });
