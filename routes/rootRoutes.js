ac2bc34f9daf89719a330830fe27a0896d7b26d8
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-09-26 16:53:22 +0530

    route chanfes

--- routes/rootRoutes.js
+++ routes/rootRoutes.js
@@ -9,8 +9,11 @@ router.use("/review", require("./review"));
 router.use("/rooms", require("./room"));
 router.use("/order", require("./orderRoutes"));
 router.use("/category", require("./categoryRoutes"));
+// router.use("/waiters", require("./waiters"));
 router.use("/waiters", require("./authWaiterRoutes"));
 router.use("/addWaiter", require("./waiter"));
+
+// router.use("/")
 
 // router.use("/")
 
