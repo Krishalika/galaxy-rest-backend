8441ed66feb4670aecf3ef52f8a05fcf81489a31
vinoja98 <vinoiarathnayake@gmail.com>
2021-09-26 16:47:40 +0530

    waiter done

--- routes/waiter.js
+++ routes/waiter.js
@@ -15,4 +15,29 @@ router.get("/me", auth, async (req, res) => {
   res.send(waiter);
 });
 
+router.post("/signup", async (req, res) => {
+  const validation = validate(req.body);
+  if (validation.error) {
+    return res.status(400).json(validation.error.details[0].message);
+  }
+
+  try {
+    let user = await Waiter.findOne({ email: req.body.email });
+    if (user) return res.status(400).json("Waiter already registered.");
+    user = new Waiter(
+      _.pick(req.body, ["name", "email", "password", "nic", "contactNo","salary"])
+    );
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+
+    const token = user.generateAuthToken();
+    res
+      .header("x-auth-token", token)
+      .json(_.pick(user, ["_id", "name", "email", "nic", "contactNo","salary"]));
+  } catch (err) {
+    return res.status(422).json(err.message);
+  }
+});
+
 module.exports = router;
