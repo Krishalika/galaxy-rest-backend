31fc1b388ace367c594703b5476db8b1781339b6
vinoja98 <vinoiarathnayake@gmail.com>
2021-10-14 21:59:39 +0530

    food delete update integration done

--- routes/food.js
+++ routes/food.js
@@ -1,4 +1,5 @@
 const auth = require("../middleware/auth");
+const validateObjectId = require('../middleware/validObjectID')
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const _ = require("lodash");
@@ -8,8 +9,8 @@ const foodController = require("../controllers/foodController");
 router.get("/by-category", foodController.getFoodByCategory);
 router.get("/", foodController.getFood);
 router.post("/add", foodController.addFood);
-router.delete("/:id", foodController.deleteFood);
-router.post("/update/:id", foodController.updateFood);
+router.delete("/:id",validateObjectId, foodController.deleteFood);
+router.post("/update/:id",validateObjectId, foodController.updateFood);
 router.get("/by-code", foodController.getFoodByCode);
 router.get("/by-name", foodController.getFoodByName);
 router.get("/by-id", foodController.getFoodByID);
