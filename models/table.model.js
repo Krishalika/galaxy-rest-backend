dc3e1e65ae1d19dcfa2c624660342fed0b68819d
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-10-18 12:36:13 +0530

    changes

--- models/table.model.js
+++ models/table.model.js
@@ -1,16 +1,5 @@
 const mongoose = require("mongoose");
 
-// const tableSchema = new mongoose.Schema(
-//   {
-//     seatCount: { type: Number, required: true },
-//     status: { type: String, required: true, minLength: 3 },
-//     tableNumber: { type: Number, required: true },
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("tables", tableSchema);
-
 const Table = mongoose.model(
   "Table",
   new mongoose.Schema(
--- routes/table.js
+++ routes/table.js
@@ -2,10 +2,14 @@ const router = require("express").Router();
 const _ = require("lodash");
 let { Table } = require("../models/table.model");
 
+const tableController = require("../controllers/tableController");
+
 router.get("/", (req, res) => {
   Table.find()
     .then((Tables) => res.json(Tables))
     .catch((err) => res.status(422).json("Error: " + err));
 });
 
+router.get("/by-id", tableController.getTableByID);
+router.get("/by-tableNo", tableController.getTableByTableNo);
 module.exports = router;
